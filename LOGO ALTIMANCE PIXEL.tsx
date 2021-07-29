f470219fad77240cab910faef3d0fd995e659fbe
DonnayAdrien <77806494+DonnayAdrien@users.noreply.github.com>
2021-07-29 19:44:09 +0200

    Create LOGO ALTIMANCE PIXEL.tsx

---
+++ LOGO ALTIMANCE PIXEL.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="LOGO ALTIMANCE PIXEL" tilewidth="30" tileheight="32" tilecount="1" columns="1">
+ <image source="LOGO ALTIMANCE PIXEL.png" width="30" height="32"/>
+</tileset>
